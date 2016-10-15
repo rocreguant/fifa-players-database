bf742f50efbe66d2410e4562c7ed2a320b3dd4f2
Roc Reguant <roc.reguant@gmail.com>
2016-10-15 15:51:51 +0200

    better version players on the initial team, for all matches with position and no prob with special characters

--- v2-lineup-parsing.py
+++ v2-lineup-parsing.py
@@ -4,6 +4,10 @@ import re
 from time import gmtime, strftime
 import unicodedata
 
+
+offset = 46
+
+
 #idea info: http://www.gregreda.com/2015/02/15/web-scraping-finding-the-api/
 
 #i don't think this is working properly
@@ -17,6 +21,19 @@ def get_lineup(match):
 	#To convert it i used the following external help:
 	#http://curl.trillworks.com/
 	#from : http://www.football-lineups.com/match/231269/
+	
+	################
+	# getting the teams
+	headers = {'User-Agent': 'Mozilla/4.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+	page = requests.get('http://www.football-lineups.com/match/'+match+'/' , headers=headers)
+	teams = re.findall('title\>([.\- A-Za-z0-9]+)La Liga',remove_accents(page.text[:400]))[0]
+	teams = teams.split(' vs ')
+	lineup = {}
+	
+	################
+	# getting the players
+	
+	
 	cookies = {
 		  '_ga': 'GA1.2.1689489604.1474738067',
 		  'PHPSESSID': 'd3ommvr4fccp4qvf4kofcqk9k7',
@@ -40,19 +57,58 @@ def get_lineup(match):
 
 	r = requests.post('http://www.football-lineups.com/ajax/get_lnp.php', headers=headers, cookies=cookies, data=data)
 
-	h = remove_accents(r.text)#unicodedata.normalize('NFKD', unicode(r.text, 'utf8'))
+	t = remove_accents(r.text)
+	sp = t.split('<script>')[2]
+	ps = sp.split('document.getElementById') # starts at 1
+	first = True
+	c = 0
+	players = {}
+	for p in ps: #iterating through all players
+		if first:
+			first = False
+			continue
+		
+		#getting in a way the id
+		_id = re.findall('[a-zA-Z0-9]+j([0-9]{1,2})\'\)',p)
+		if not _id:
+			continue
+		#get name
+		
+		name = re.findall('title=\"([a-zA-Z0-9\s,\-.]+)\"',p)[0]
+		position = re.findall('class=\"([a-zA-Z]{4})\"',p)[0]
+		player = {"name":name, "position":position}
+		c +=1
+		players[_id[0]] = player
+		if c  == 11:
+			lineup[teams[0]] = players
+			#c = 0
+			players = {}
+	lineup[teams[1]] = players
 	
-	aux = re.findall('class="[A-Za-z]+"\s title="(.+)"\s href="\/footba',h)[0]
-	print re.findall('class="[A-Za-z]+"\s+title="([\w\s]*)"',aux)
+	return lineup
 
 
-print get_lineup('231269')
+headers = {'User-Agent': 'Mozilla/4.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+page = requests.get('http://www.football-lineups.com/tourn/La_Liga_2015-2016/fixture/' , headers=headers)
+
+#TODO check if the server returned a page or said that was overloaded
+
+matches = re.findall('<td align=center width=50 ><a  href="/match/([0-9]+)/"',page.content)
+
+i = 0
+for match in matches:
+	i+=1
+	
+	#added offset continuity
+	if i < offset:
+		continue
+	print i, "out of 380 ", match
+
+	output = get_lineup(str(match))
+	
+	#saving the match
+	with open('lineup-data2/'+str(match)+'.json', 'w') as outfile:
+			json.dump(output, outfile)
 
 
-
-#first step getting all the players
-#class="[A-Za-z]+"\s title="(.+)"\s href="\/footba
-
-#getting the names
-#class="[A-Za-z]+"\s+title="([\w\s]*)"
-
+print "done!!"
